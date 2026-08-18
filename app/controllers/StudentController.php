4fefad7293be4af2bad51d116ced882879b5bf1b
angelbuligen-gel <angelbuligen@gmail.com>
2026-08-18 23:52:21 +0800

    o

--- app/controllers/StudentController.php
+++ app/controllers/StudentController.php
@@ -4,13 +4,7 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 
 class StudentController extends Controller
 {
-    public function __construct()
-    {
-        if (session_status() === PHP_SESSION_NONE) {
-            session_start();
-        }
-    }
-
+    
     public function index()
     {
         $_SESSION['student_access'] = true;
