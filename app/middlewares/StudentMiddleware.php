1e92a9c6e886afa775a3721130ab83baeb4759d7
angelbuligen-gel <angelbuligen@gmail.com>
2026-08-18 23:32:40 +0800

    o

--- app/middlewares/StudentMiddleware.php
+++ app/middlewares/StudentMiddleware.php
@@ -1,18 +1,16 @@
-    <?php
+<?php
 
-    defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
+defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 
-    class StudentMiddleware
+class StudentMiddleware
+{
+    public function handle(Closure $next)
     {
-        public function handle(Closure $next)
-        {
-           if (session_status() === PHP_SESSION_NONE) {
-            session_start();
+        if (!isset($_SESSION['student_access']) || $_SESSION['student_access'] !== true) {
+            echo "You can't access this page.";
+            exit;
         }
-            if (!isset($_SESSION['student_access']) || $_SESSION['student_access'] !== true) {
-                echo "You can't access this page.";
-                exit;
-            }
-            return $next();
-        }
+
+        return $next();
     }
+}
