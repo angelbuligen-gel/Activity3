89de3a7dc47a7d1681c1ee0fcc1783d607e5f296
angelbuligen-gel <angelbuligen@gmail.com>
2026-08-18 23:29:13 +0800

    s

--- app/middlewares/StudentMiddleware.php
+++ app/middlewares/StudentMiddleware.php
@@ -6,7 +6,9 @@
     {
         public function handle(Closure $next)
         {
-           
+           if (session_status() === PHP_SESSION_NONE) {
+            session_start();
+        }
             if (!isset($_SESSION['student_access']) || $_SESSION['student_access'] !== true) {
                 echo "You can't access this page.";
                 exit;
